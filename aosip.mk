112c994006a82d310f11cc3eebba520aef0f70ff
Gumball1506 <38982972+Gumball1506@users.noreply.github.com>
2018-05-24 09:35:54 +0700

    Update and rename aicp.mk to aosip.mk

--- aosip.mk
+++ aosip.mk
@@ -8,8 +8,8 @@ TARGET_SCREEN_HEIGHT := 960
 #Bootanimation Res
 TARGET_BOOT_ANIMATION_RES := 540
 
-# Inherit some common AICP stuff.
-$(call inherit-product, vendor/aicp/config/common_full_phone.mk)
+# Inherit some common AOSiP stuff.
+$(call inherit-product, vendor/aosip/config/common_full_phone.mk)
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
