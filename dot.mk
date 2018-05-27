e339312ec43c6a91e58a34386c2eb04dae928840
Real man born on semtember <hieunam03@gmail.com>
2018-05-27 08:41:18 +0700

    Create dot.mk

---
+++ dot.mk
@@ -0,0 +1,20 @@
+# Release name
+PRODUCT_RELEASE_NAME := Samsung Galaxy Grand Prime
+
+# Boot animation
+TARGET_SCREEN_WIDTH := 540
+TARGET_SCREEN_HEIGHT := 960
+
+#Bootanimation Res
+TARGET_BOOT_ANIMATION_RES := 540
+
+# Inherit some common AEX stuff
+$(call inherit-product, vendor/dot/config/common.mk)
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_CHARACTERISTICS := phone
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
