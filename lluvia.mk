b664b2e6ae86387f58e8fe6b9f86722b5f3019d0
Gumball1506 <38982972+Gumball1506@users.noreply.github.com>
2018-05-28 07:10:24 +0700

    Update and rename fh.mk to lluvia.mk

--- lluvia.mk
+++ lluvia.mk
@@ -8,8 +8,9 @@ TARGET_SCREEN_HEIGHT := 960
 #Bootanimation Res
 TARGET_BOOT_ANIMATION_RES := 540
 
-# Inherit some common Candy stuff.
-$(call inherit-product, vendor/fh/config/common_full_phone.mk)
+# Inherit some common LLuviaOS stuff.
+$(call inherit-product, vendor/lluvia/config/common_full_phone.mk)
+$(call inherit-product, vendor/lluvia/config/caf_fw.mk)
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
